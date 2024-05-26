9d402aab87888c6d995f31ad7823e032bdaf9a73
mumtozBEK1234 <email manzilingiz>
2024-05-26 18:26:03 +0300

    4-oy 5dars

---
+++ index.js
@@ -0,0 +1,101 @@
+// 1 masala
+// let n = 5
+
+// for (let i = 1 ; i <= n; i++ ) {
+//     console.log(i);
+    
+// }
+
+// 2 masala 
+
+// let n = 10
+//  for (let i = n ; i >= 1; i-- ) {
+//     console.log(i);
+//  }
+
+// 3-masala
+
+// let n = 10 
+
+// let result = 0
+// for (let i = 1 ; i <= n ; i++) {
+//   result  += i 
+// }
+// console.log(result);
+
+
+
+
+// 4-MASALA
+// let n = 10 
+
+// for(let i)
+// for-1 masala 
+
+// let k = 20; 
+// let n = 10;
+
+// for (let i = 0; i <= n; i++ ) {
+//     console.log(k);
+    
+// }
+
+// for-2 masala
+
+// let a = 2
+// let b = 4
+// for(a; a <= b; a++){
+//     console.log(a);
+// }
+
+// for-3 masala
+
+// let a = 2
+// let b = 5
+
+// for (let i = b - 1; i > a; i-- ) {
+//     console.log(i);
+    
+// } 
+
+// for-4masala
+
+// let kg = 10
+
+// for (let i = 1 ; i <= kg; i++ ) {
+//     console.log(i * kg);
+    
+// }
+
+// for-5 MASALA
+
+// const Kg = 1000; 
+
+// for (let i = 1; i <= 10; i++) {
+//     const son = i / 10;
+//     const result = son * Kg;
+//     console.log(`${son} kg konfetning narxi: ${result} som`);
+// }
+
+// for-6 masala
+
+// const kg = 1000;
+
+
+// for(let i = 10 ; i<= 20 ; i++) {
+//     const son = i / 10;
+//     const result =  son * kg 
+//     console.log(` ${son} kg konfetning narxi: ${result} so'm `);
+// }
+
+// for-7 masala
+// let a = 1
+// let b = 10
+// let result = 0 
+
+
+// for (let i = a ; i <= b; i++ ) {
+//    result = result + i;
+    
+// }
+// console.log(result);
